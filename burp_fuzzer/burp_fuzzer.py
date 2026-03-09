eaab8b17287a01a5980260a6d0762c8e0039984b
Zauzanov <zauzanov@gmail.com>
2026-03-10 02:03:42 +0300

    Add BurpExtender class

--- burp_fuzzer/burp_fuzzer.py
+++ burp_fuzzer/burp_fuzzer.py
@@ -6,3 +6,17 @@ from java.util import List, ArrayList
 
 import random
 
+class BurpExtender(IBurpExtender, IIntruderPayloadGeneratorFactory):
+    def registerExtenderCallbacks(self, callbacks):
+        self._callbacks = callbacks
+        self._helpers = callbacks.getHelpers()
+
+        callbacks.registerIntruderPayloadGeneratorFactory(self)
+        return
+    
+    def getGeneratorName(self):
+        return "Payload Generator"
+    
+    def createNewInstance(self, attack):
+        return BurpFuzzer(self, attack)
+    
