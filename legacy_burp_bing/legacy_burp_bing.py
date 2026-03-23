cafa2a316b2ba13fcee7f6b929145fe99bccda9b
Zauzanov <zauzanov@gmail.com>
2026-03-23 14:57:44 +0300

    Create a directory

---
+++ legacy_burp_bing/legacy_burp_bing.py
@@ -0,0 +1,14 @@
+from burp import IBurpExtender
+from burp import IContextMenuFactory
+
+from java.net import URL
+from java.util import ArrayList
+from javax.swing import JMenuItem
+from thread import start_new_thread
+
+import json
+import socket 
+import urllib
+API_KEY = "YOUR_KEY"
+API_HOST = "api.cognitive.microsoft.com"
+
