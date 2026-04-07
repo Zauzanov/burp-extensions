b6b0bc25cd07ad736c5c6c1229e48ae86c1845ae
Zauzanov <zauzanov@gmail.com>
2026-04-07 10:31:49 +0300

    Import modules

--- burp_website_wordlist_generator/wwg.py
+++ burp_website_wordlist_generator/wwg.py
@@ -1,1 +1,11 @@
 from burp import IBurpExtender
+from burp import IContextMenuFactory
+
+from java.util import ArrayList
+from javax.swing import JmenuItem
+
+from datetime import datetime
+from HTMLParser import HTMLParser
+
+import re 
+
